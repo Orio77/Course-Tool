25be26c231cef83e8ce340da4b5472c0fed28181
Orio <maciejll@wp.pl>
2024-07-18 18:49:16 +0200

    feat: Add logging statements for webhook events

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -8,23 +8,31 @@ export async function POST(req: Request) {
     const body = await req.text()
     const signature = headers().get('Stripe-Signature') as string
     let event: Stripe.Event
+    console.log("Beginning of the webhook")
 
     try {
         event = stripe.webhooks.constructEvent(
             body, signature,
             process.env.STRIPE_WEBHOOK_SECRET as string
         )
+        console.log("First rty catch")
     } catch (error: any) {
+        console.log("Error occurred")
         return new NextResponse('webhook error', {status: 400})
     }
 
     const session = event.data.object as Stripe.Checkout.Session
+    console.log("Session created: ")
+    console.log(session)
 
     if (event.type === 'checkout.session.completed') {
+        console.log("Checkout session completed")
         const subscription = await stripe.subscriptions.retrieve(session.subscription as string)
         if (!session?.metadata?.userId) {
+            console.log("No user id")
             return new NextResponse('webhook error, no userId', {status:400})
         }
+        console.log("Continuing")
         await prisma.userSubscription.create({
             data: {
                 userId: session.metadata.userId,
@@ -36,10 +44,13 @@ export async function POST(req: Request) {
                 )
             }
         })
+        console.log("User subscription created")
     }
-
+    console.log("Between if's")
     if (event.type === 'invoice.payment_succeeded') {
+        console.log("Entered second if")
         const subscription = await stripe.subscriptions.retrieve(session.subscription as string)
+        console.log("Subscription created")
         await prisma.userSubscription.update({
             where: {
                 stripeSubscriptionId: subscription.id
@@ -52,6 +63,7 @@ export async function POST(req: Request) {
             }
         })
     }
+    console.log("returning")
     return new NextResponse(null, {status: 200})
 }
 
--- src/components/SubscriptionAction.tsx
+++ src/components/SubscriptionAction.tsx
@@ -27,7 +27,7 @@ const SubscriptionAction = (props: Props) => {
     <div className='flex flex-col items-center w-1/2 p-4 mx-auto mt-4 rounded-md bg-secondary'>
         {data?.user.credits} / 10 Free Generations
         <Progress className='mt-2' value={data?.user.credits ? (data.user.credits / 10) * 100 : 0}/>
-        <Button onClick={handleSubscribe} className='mt-3 font-bold text-white transition bg-gradient-to-tr from-green-400 to-blue-400 hover:from-green-500 hover:to-blue-600'>
+        <Button disabled={loading} onClick={handleSubscribe} className='mt-3 font-bold text-white transition bg-gradient-to-tr from-green-400 to-blue-400 hover:from-green-500 hover:to-blue-600'>
             Upgrade
             <Zap className='fill-white ml-2'/>
         </Button>

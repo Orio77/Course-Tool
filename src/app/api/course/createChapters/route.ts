9b79dd23f5149b8b51d9f6c525596c1d78cfd05e
Orio <maciejll@wp.pl>
2024-07-24 20:39:45 +0200

    feat: Add user ID to course creation data

--- src/app/api/course/createChapters/route.ts
+++ src/app/api/course/createChapters/route.ts
@@ -74,6 +74,7 @@ export async function POST(req: Request, res: Response) {
             data: {
                 name: title,
                 image: course_image,
+                id: session.user.id
             },
         });
 
